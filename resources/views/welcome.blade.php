7fc1e5ff2f1a49ade5c95ee6d249cb71d174d613
marickosheiy <marickosheiy@gmail.com>
2026-08-07 22:34:04 +0800

    Initial Laravel installation

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Homepage</title>
+</head>
+<body>
+
+    <h1>Welcome to My Laravel Project</h1>
+
+    <p><strong>Student Name:</strong> Maricko Sheiy L. Villacorta</p>
+    <p><strong>Student Number:</strong> 0124-1151</p>
+    <p><strong>Course:</strong> BS Information Technology</p>
+    <p><strong>Section:</strong> BSIT 3A </p>
+    <p><strong>Subject:</strong> Client-Server Technologies</p>
+    <p><strong>Current Date:</strong> August 7, 2026</p>
+
+    <hr>
+
+    <footer>
+        <p>Created using Laravel 12</p>
+    </footer>
+
+</body>
+</html>
